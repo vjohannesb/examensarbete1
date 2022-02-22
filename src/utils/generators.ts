0ec508edb2e004f6cb6ddaec22d413290725028a
Johannes Bergendahl <johannes.bergendahl@tiqqe.com>
2022-02-22 19:36:39 +0100

    ✨  Chord & Note Generators

---
+++ src/utils/generators.ts
@@ -0,0 +1,47 @@
+import store from "@/store";
+import { Note } from "./tone/generator";
+
+export function generateChord(): string[] {
+    const notes = store.state.notesInKey;
+
+    // Between 3 and 6
+    let octave = Math.floor(Math.random() * 3) + 3;
+
+    const rootIndex = Math.floor(Math.random() * notes.length);
+    const rootNote = notes[rootIndex] + octave;
+
+    const secondIndex = (rootIndex + 2) % notes.length;
+    if (secondIndex < rootIndex) octave += 1;
+    const secondNote = notes[secondIndex] + octave;
+
+    const thirdIndex = (secondIndex + 2) % notes.length;
+    if (thirdIndex < secondIndex) octave += 1;
+    const thirdNote = notes[thirdIndex] + octave;
+
+    const chord = [rootNote, secondNote, thirdNote];
+
+    if (Math.random() > 0.5) {
+        const fourthIndex = (thirdIndex + 2) % notes.length;
+        if (fourthIndex < thirdIndex) octave += 1;
+        const fourthNote = notes[fourthIndex] + octave;
+
+        chord.push(fourthNote);
+
+        if (Math.random() > 0.5) {
+            const fifthIndex = (fourthIndex + 2) % notes.length;
+            if (fifthIndex < thirdIndex) octave += 1;
+            const fifthNote = notes[fifthIndex] + octave;
+
+            chord.push(fifthNote);
+        }
+    }
+    return chord;
+}
+
+export function getNextNote(): Note {
+    const notes = store.state.notesInKey;
+    const octave = Math.floor(Math.random() * 3) + 3;
+
+    const rndIndex = Math.floor(Math.random() * notes.length);
+    return notes[rndIndex] + octave;
+}
